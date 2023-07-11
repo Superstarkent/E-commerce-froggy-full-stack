141874bb694177507a5eb997de97816e9c99fac6
William <supercerealwill@gmail.com>
2023-07-11 16:47:43 +0200

    update

--- api/src/services/order.ts
+++ api/src/services/order.ts
@@ -3,9 +3,5 @@ import Order, { OrderDocument } from "../models/order";
 export const createOrderService = async (
   order: OrderDocument
 ): Promise<OrderDocument | undefined> => {
-  try {
     return order.save();
-  } catch (error) {
-    console.log(error);
-  }
 };
